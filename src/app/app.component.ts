0d7463213c8c22183e2b1de490a84e45d21f03f5
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-04-16 22:14:42 +0300

    add tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Gior';
+
+  person = {
+    givenName: 'GIOR',
+    surName: 'Kara',
+    age:42,
+    email:'kara@aueb.gr'
+  }
 }
