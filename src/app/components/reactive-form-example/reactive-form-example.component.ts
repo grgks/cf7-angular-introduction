df11de79dcb05d69442b7330d4aed99f16fa91f6
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-05-07 16:52:47 +0300

    fixes

--- src/app/components/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/reactive-form-example/reactive-form-example.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { PersonTableComponent } from '../person-table/person-table.component';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
 import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-reactive-form.component';
+import { PersonService } from 'src/app/shared/services/person.service';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
+
 
 @Component({
   selector: 'app-reactive-form-example',
@@ -14,5 +17,15 @@ import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-r
   styleUrl: './reactive-form-example.component.css'
 })
 export class ReactiveFormExampleComponent {
+  personService = inject(PersonService);
+  currentPerson: EPerson | undefined;
+  persons: EPerson[] = [];
 
+  onPerson(data: EPerson){
+    // console.log("Father", data);
+    this.currentPerson = data;
+    this.persons.push(data);
+    this.personService.modifiedDataTable.set(true)
+    // console.log("Father", this.persons);
+  }
 }
