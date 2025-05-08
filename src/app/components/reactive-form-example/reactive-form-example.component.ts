7a6527471f86046fe4a83b2fa0cc10e61352cd39
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-05-08 23:35:40 +0300

    HTTP Client Example, User Registration Component

--- src/app/components/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/reactive-form-example/reactive-form-example.component.ts
@@ -2,7 +2,7 @@ import { Component,inject } from '@angular/core';
 import { PersonTableComponent } from '../person-table/person-table.component';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
 import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-reactive-form.component';
-import { PersonService } from 'src/app/shared/services/person.service';
+//import { PersonService } from 'src/app/shared/services/person.service';
 import { EPerson } from 'src/app/shared/interfaces/eperson';
 
 
@@ -17,15 +17,16 @@ import { EPerson } from 'src/app/shared/interfaces/eperson';
   styleUrl: './reactive-form-example.component.css'
 })
 export class ReactiveFormExampleComponent {
-  personService = inject(PersonService);
+  //personService = inject(PersonService);
   currentPerson: EPerson | undefined;
   persons: EPerson[] = [];
 
   onPerson(data: EPerson){
     // console.log("Father", data);
     this.currentPerson = data;
-    this.persons.push(data);
-    this.personService.modifiedDataTable.set(true)
+    //this.persons.push(data);
+    this.persons = [...this.persons, data]
+    //this.personService.modifiedDataTable.set(true)
     // console.log("Father", this.persons);
   }
 }
