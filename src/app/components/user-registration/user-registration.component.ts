b297e6769ff26c18722b6633ac757eba83ccf168
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-05-12 23:49:12 +0300

    add verifications for  Password/Email

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { 
+  AbstractControl,
   FormControl, 
   FormGroup, 
   ReactiveFormsModule, 
   Validators 
 } from '@angular/forms';
+import { UserService } from 'src/app/shared/services/user.service';
+import { User } from 'src/app/shared/interfaces/user';
+
 
 @Component({
   selector: 'app-user-registration',
@@ -21,6 +25,14 @@ import {
   styleUrl: './user-registration.component.css'
 })
 export class UserRegistrationComponent {
+   userService = inject(UserService)
+
+  emailErrorMessage = signal('');
+
+  registrationStatus: {success: boolean, message: string} = {
+    success: false,
+    message: 'Not attempted yet'
+  }
 
   form = new FormGroup({
     username: new FormControl('', Validators.required),
@@ -33,5 +45,84 @@ export class UserRegistrationComponent {
     }),
     password: new FormControl('', [Validators.required, Validators.minLength(5)]),
     confirmPassword: new FormControl('', [Validators.required, Validators.minLength(5)])
-  })
+  },
+   this.passwordConfirmValidator,
+);
+ passwordConfirmValidator(control: AbstractControl): {[key:string]: boolean} | null {
+    const form = control as FormGroup;
+    
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value
+
+    if(password && confirmPassword && password!==confirmPassword) {
+      form.get('confirmPassword')?.setErrors({passwordMismatch: true})
+      return {passwordMismatch: true}
+    }
+    
+    return null
+  }
+
+  onSubmit(){
+    // const data = this.form.value as User;
+    const data: User = {
+      'username': this.form.get('username')?.value || '',
+      'password': this.form.get('password')?.value || '',
+      'name': this.form.get('name')?.value || '',
+      'surname': this.form.get('surname')?.value || '',
+      'email':this.form.get('email')?.value || '',
+      'address': {
+        'area':this.form.get('area')?.value || '',
+        'road': this.form.get('road')?.value || ''
+      }
+    }
+    console.log(data);
+    this.userService.registerUser(data)
+      .subscribe({
+        next: (response) => {
+          console.log("User Saved", response);
+          this.registrationStatus = {success: true, message: "User registrered"}
+        },
+        error: (response) => {
+          console.log("User not Saved", response)
+          this.registrationStatus = {success: false, message: response.data}
+        }
+      })
+    
+  }
+
+  check_dublicate_email(){
+    const email = this.form.get("email")?.value;
+
+    if (this.form.controls.email.hasError('required')){
+      this.emailErrorMessage.set('Email is required')
+    } else if (this.form.controls.email.hasError('email')){
+      this.emailErrorMessage.set('Email not valid')
+    }
+
+    if (email){
+      console.log("email", email);
+      this.userService.check_dublicate_email(email)
+        .subscribe({
+          next: (response) => {
+            console.log("Email OK",response);
+            // this.form.get("email")?.setErrors(null)
+            // this.emailErrorMessage.set('')
+            this.emailErrorMessage.set('Email not exists')
+          },
+          error: (response) => {
+            console.log(response);
+            const message = response.data;
+            console.log("Email not OK",message);
+            // this.form.get('email')?.setErrors({dublicateEmail: true})
+            this.emailErrorMessage.set('Email exists')
+          }
+        })
+    }
+  }
+
+  registerAnother(){
+    this.form.reset()
+    this.registrationStatus = {success:false, message: "Not attempted yet"}
+  }
 }
+
